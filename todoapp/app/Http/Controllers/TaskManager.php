96c78f0c389a65a7f7fe0396c160922918c13d49
Uththara Jayawardana <gaveuththara1999@gmail.com>
2025-01-16 16:15:28 +0530

    Created Task Manager

---
+++ todoapp/app/Http/Controllers/TaskManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskManager extends Controller
+{
+    function addTask()
+    {
+        return view('tasks.add');
+    }
+}
--- todoapp/resources/views/layouts/default.blade.php
+++ todoapp/resources/views/layouts/default.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" class="h-100">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,10 +7,11 @@
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
     @yield("style")
   </head>
-  <body>
+  <body class="d-flex flex-column h-100">
     <div>
       @include("include.header")
       @yield("content")
+      @include("include.footer")
     </div>
     <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
   </body>
