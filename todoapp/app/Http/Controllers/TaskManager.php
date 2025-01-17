bfdf75fc9a5c4ddd8be5204e048ff1d6b7e95583
Uththara Jayawardana <gaveuththara1999@gmail.com>
2025-01-17 10:46:13 +0530

    Created adding tasks

--- todoapp/app/Http/Controllers/TaskManager.php
+++ todoapp/app/Http/Controllers/TaskManager.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Tasks;
 
 use Illuminate\Http\Request;
 
@@ -13,9 +14,16 @@ class TaskManager extends Controller
 
     function addTaskPost(Request $request){
         $request->validate([
-           'title' => 'required' ,
+           'title' => 'required',
            'description' => 'required',
            'deadline' => 'required',
         ]);
+        $task = new Tasks();
+        $task->title = $request->title;
+        $task->description = $request->description;
+        $task->deadline = $request->deadline;
+        if($task->save()){
+            return redirect(route("tasks"));
+        }
     }
 }
